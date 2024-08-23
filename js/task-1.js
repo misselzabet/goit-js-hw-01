b48fc5d4cdf50d937b415dd610c35e1032134867
misselzabet <lisa.kovalevskaya1999@gmail.com>
2024-08-23 14:15:22 +0200

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const quantity = undefined;
+const pricePerDroid = undefined;
+
+function makeTransactilon (quantity, pricePerDroid)
+{
+  console.log( `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
+ }
+ makeTransactilon();
+
+
+
+
